b0b3ad8855500858058bf1fd4b02ee38f5c0e742
einavobp <p.einavoss@gmail.com>
2017-08-26 15:53:33 +0300

    added settings fragment

--- app/src/main/java/proj/sadna/mta/sadna_2017/app/Fragments/UserSettingsFragment.java
+++ app/src/main/java/proj/sadna/mta/sadna_2017/app/Fragments/UserSettingsFragment.java
@@ -1,10 +1,13 @@
 package proj.sadna.mta.sadna_2017.app.Fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 import proj.sadna.mta.sadna_2017.R;
 
@@ -13,6 +16,13 @@ import proj.sadna.mta.sadna_2017.R;
  */
 public class UserSettingsFragment extends Fragment
 {
+    Activity context;
+    RelativeLayout feedback;
+    RelativeLayout notification;
+    RelativeLayout profile;
+    RelativeLayout logout;
+    Switch mSwitch;
+    private RelativeLayout share;
 
     public UserSettingsFragment()
     {
